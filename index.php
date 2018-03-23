234c9c4e11fe4a5f5fcd5c6ccdd6e91ab5852695
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-23 16:40:44 +0300

    Первый коммит. Создал единную точку доступа. Создал папку app, в которой будут находится папки приложения. В ней создал папку public для статической отдачи файлов стилей и джаваскрипт.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Fraemwork</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link rel="stylesheet" type="text/css" href="/public/css/style.css">
+	<script type="text/javascript" src="/public/js/scripts.js"></script>
+</head>
+<body>
+	<h1>Fraemwork</h1>
+</body>
