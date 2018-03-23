b64b7cc5385f41da309046988d0084b82448a137
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-23 18:35:02 +0300

    Создание файла constants.php для хранения всех констант приложения.

---
+++ constants.php
@@ -0,0 +1,7 @@
+<?php
+// определяем корневую директорию проекта
+defined('ROOT') or define('ROOT', __DIR__);
+// директория приложения
+defined('APPDIR') or define('APPDIR', ROOT . '/app');
+// директория фраемворка
+defined('FRAEMWORKDIR') or define('FRAEMWORKDIR', ROOT . '/vendor/fraemwork');
