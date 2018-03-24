82415d355bb03c81cf220f57085d3a72287736b1
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-24 10:18:49 +0300

    Добавил простой фавикон

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+use fraemwork\errors\ErrorsHandler;
+
 // вставляем файл констант
 require_once 'constants.php';
 // инициализируем автозагрузчик классов
 require_once 'autoload.php';
+
+$error = new ErrorsHandler();
+echo $test;
--- vendor/fraemwork/errors/views/error.php
+++ vendor/fraemwork/errors/views/error.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="/public/favicon.ico" type="image/x-icon">
     <title>Ошибка</title>
 	<link rel="stylesheet" type="text/css" href="/public/css/bootstrap.min.css">
 	<style>
