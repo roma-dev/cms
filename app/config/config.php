78b4ad1f05e37d303b8f53878ade5fb6c7ffc36d
Roma Malikov <roma.dev.05@yandex.ru>
2018-03-24 15:50:42 +0300

    Обработчик ошибок: В файле конфигурации /config/config.php создал массив данных для инициализации обработчика ошибок.

--- app/config/config.php
+++ app/config/config.php
@@ -2,4 +2,22 @@
 
 return [
 	
+	'errorHandler' => // настройки обработчика ошибок
+	[
+		'logs' => false, // будем логировать ошибки или нет
+		'logFiles' => // в какие файлы и при каких ошибках
+		[
+			'403' => '/logs/forbbiden.log',
+			'404' => '/logs/notfound.log',
+			'500' => '/logs/errors.log',
+		],
+		'errorsView' => // файлы видов отображения ошибок для фронтенда
+		[
+			'403' => '/views/errors/e403.php',
+			'404' => '/views/errors/e404.php',
+			'500' => '/views/errors/e500.php',
+			'503' => '/views/errors/e503.php',
+		]
+	],
+	
 ];
